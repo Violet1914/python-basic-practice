a482eb9e2cad883e6b5833c78612ffbbce9402f1
Violet1914 <1914395403@qq.com>
2026-08-06 13:11:02 +0800

    day1

---
+++ test01.py
@@ -0,0 +1,16 @@
+name = "1234itheima itcast boxuegu1234"
+
+name1 = name.count("it")
+
+print(name1)
+
+name2 = name.split(" ")
+
+print(name2)
+
+name3 = name.replace(" ","|")
+print(name3)
+
+name4 = name.strip("123")
+
+print(name4)

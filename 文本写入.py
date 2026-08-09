4f7419305abdb41e307c730eca81049b01b4477c
Violet1914 <1914395403@qq.com>
2026-08-09 20:58:55 +0800

    day4-文件

---
+++ 文本写入.py
@@ -0,0 +1,24 @@
+f = open("C:\\Users\\ZhuanZ\\Desktop\\word.txt", "r", encoding="utf-8")
+"""count = 0
+for line in f:
+    print(line)
+    print(type(line))
+    list = line.strip("\n")
+    list1 = list.split(" ")
+    for word in list1:
+        print(word)
+        if word == "itheima":
+            count += 1
+print(count)
+f.close()
+"""
+
+content = f.read()
+
+str = content.split("\n")
+
+print(str, type(str))
+
+count = content.count("itheima")
+
+print(count)
